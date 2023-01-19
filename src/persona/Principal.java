475ca33ec746c22d918d63847e23ad3491724d40
Nicolas Mamone <nicolasmamone@gmail.com>
2023-01-19 16:45:39 -0300

    En la clase principal instanciamos un cliente

--- src/persona/Principal.java
+++ src/persona/Principal.java
@@ -8,6 +8,7 @@ public class Principal {
         Persona persona3 = new Persona("felipe", 34);
 
         Empleado empleado1 = new Empleado("Maria", 43, "00001");
+        Cliente cliente1 = new Cliente("Pedro", 45, "Calle falsa 123");
 
         System.out.println(persona2.getCantidadDePersonas());
         System.out.println(Persona.getCantidadDePersonas());
